b1a911b44190184e07ceaf54a5994c088d174ce1
mingdong <tumingdong@sdbattery.com>
2021-03-11 17:04:48 +0800

    项目搭建

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import router from "./router/index"
+import App from './App.vue';
+
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
