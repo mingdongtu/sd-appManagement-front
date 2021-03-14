72f30ab55ba5cd1ab14472e28804420b8923a554
mingdong <tumingdong@sdbattery.com>
2021-03-14 18:55:37 +0800

    提交

--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -1,131 +1,113 @@
-
-  
 <template>
-<el-container style="height: 800px; border: 1px solid #eee">
-<!-- 菜单栏-->
-  <el-aside width="200px" style="background-color: rgb(44, 48, 58)">
-    <el-menu 
-     background-color='rgb(44, 48, 58)'
-     text-color="#fff"
-     active-text-color="#000"
-     @select="handleSelect"
-     >
-      <el-menu-item index="首页"  style='color:#fff;width:200px' >
-        <template #title ><i class="el-icon-s-home"></i>首页</template>
-      </el-menu-item>
-       <el-menu-item index="控制台" style='color:#fff;width:200px' >
-        <template #title ><i class="el-icon-picture-outline-round"></i>控制台</template>
-      </el-menu-item>
-    <el-submenu index="1" >
-        <template #title>
-          <i class="el-icon-location"></i>
-          <span>应用</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="合伙人APP" >合伙人APP</el-menu-item>
-          <el-menu-item index="星云APP" >星云APP</el-menu-item>
-           <el-menu-item index="IBO" >IBO</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-    </el-menu>
-     
-  </el-aside>
-<!-- 顶部栏-->
-  <el-container>
-    <el-header style="text-align: right; font-size: 12px;height:56px;background:#fff">
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <template #dropdown>
-          <el-dropdown-menu>
-              <el-dropdown-item>我的</el-dropdown-item>
-              <el-dropdown-item>退出</el-dropdown-item>
-              
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
-      <span>图图</span>
-    </el-header>
-<!-- 主要内容区-->
-    <el-main>
-       <!-- 菜单打开标签页--> 
-      <el-tabs v-model="editableTabsValue" type="border-card" closable @tab-remove="removeTab" >
-        <el-tab-pane
-          v-for="(item) in editableTabs"
-          :key="item.name"
-          :label="item.title"
-          :name="item.name"
-        >
-        
-          <router-view></router-view>
-        </el-tab-pane>
-      </el-tabs>
-    </el-main>
-    <!--<el-footer>Footer</el-footer>-->
-  </el-container>
-   
-</el-container>
+  <a-layout>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys1"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">nav 1</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <a-breadcrumb style="margin: 16px 0">
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item>List</a-breadcrumb-item>
+        <a-breadcrumb-item>App</a-breadcrumb-item>
+      </a-breadcrumb>
+      <a-layout style="padding: 24px 0; background: #fff">
+        <a-layout-sider width="200" style="background: #fff">
+          <a-menu
+            mode="inline"
+            v-model:selectedKeys="selectedKeys2"
+            v-model:openKeys="openKeys"
+            style="height: 100%"
+          >
+            <a-sub-menu key="sub1">
+              <template #title>
+                <span>
+                  <user-outlined />
+                  subnav 1
+                </span>
+              </template>
+              <a-menu-item key="1">option1</a-menu-item>
+              <a-menu-item key="2">option2</a-menu-item>
+              <a-menu-item key="3">option3</a-menu-item>
+              <a-menu-item key="4">option4</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+              <template #title>
+                <span>
+                  <laptop-outlined />
+                  subnav 2
+                </span>
+              </template>
+              <a-menu-item key="5">option5</a-menu-item>
+              <a-menu-item key="6">option6</a-menu-item>
+              <a-menu-item key="7">option7</a-menu-item>
+              <a-menu-item key="8">option8</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub3">
+              <template #title>
+                <span>
+                  <notification-outlined />
+                  subnav 3
+                </span>
+              </template>
+              <a-menu-item key="9">option9</a-menu-item>
+              <a-menu-item key="10">option10</a-menu-item>
+              <a-menu-item key="11">option11</a-menu-item>
+              <a-menu-item key="12">option12</a-menu-item>
+            </a-sub-menu>
+          </a-menu>
+        </a-layout-sider>
+        <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+          Content
+        </a-layout-content>
+      </a-layout>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
-
-
-<script lang='ts'>
-  export default {
-    data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
-      return {
-        tableData: Array(20).fill(item),
-        editableTabsValue: '2',
-        editableTabs:[
-        //   {
-        //   title: 'Tab 1',
-        //   name: '1',
-        //   content: 'Tab 1 content'
-        // }, {
-        //   title: 'Tab 2',
-        //   name: '2',
-        //   content: 'Tab 2 content'
-        // }
-        ],
-        tabIndex: 2
-      }
-    },
-    methods:{
-      handleSelect(index,indexPath){
-        console.log('选中菜单',index,indexPath)
-        // let newTabName = ++this.tabIndex + '';
-        const isHas = this.editableTabs.some(item=>item.name===index)
-        if(!isHas){
-            this.editableTabs.push({
-          title: index,
-          name: index,
-          content: 'New Tab content'
-        }); 
-        }
-        
-        this.editableTabsValue = index;
-      },
-     
-      removeTab(targetName){
-           this.editableTabs = this.editableTabs.filter(item=>item.name!==targetName)
-      }
-    }
-  };
+<script lang="ts">
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  components: {
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+  },
+  setup() {
+    return {
+      selectedKeys1: ref<string[]>(['2']),
+      selectedKeys2: ref<string[]>(['1']),
+      openKeys: ref<string[]>(['sub1']),
+    };
+  },
+});
 </script>
+<style>
+#components-layout-demo-top-side .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
 
+.ant-row-rtl #components-layout-demo-top-side .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
 
-<style scoped>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-
-  .el-aside {
-    color: #333;
-    background-color: rgb(44, 48, 58);
-  }
-  
+.site-layout-background {
+  background: #fff;
+}
 </style>
