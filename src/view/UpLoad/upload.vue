f7db74b306d2a6d8e4b4d1ce1ee9a275fb40de89
mingdong <tumingdong@sdbattery.com>
2021-03-27 14:31:09 +0800

    文件解析

--- src/view/UpLoad/upload.vue
+++ src/view/UpLoad/upload.vue
@@ -1,54 +1,45 @@
 <template>
-  <a-upload-dragger
-    v-model:fileList="fileList"
-    name="file"
-    :multiple="true"
-    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-    @change="handleChange"
-  >
-    <p class="ant-upload-drag-icon">
-      <inbox-outlined></inbox-outlined>
-    </p>
-    <p class="ant-upload-text">立刻上传</p>
-    <p class="ant-upload-hint">
-      点击按钮选择应用的安装包，或拖拽文件到此区域
-（支持ipa或apk文件，支持中断后续传）
-    </p>
-  </a-upload-dragger>
+    <div>
+      <input
+      style="margin: 10px"
+      type="file"
+      name="上传"
+      id=""
+      ref="uploadFile"
+      @change="handleChange"
+    />
+    </div>
 </template>
 <script lang="ts">
-import { InboxOutlined } from '@ant-design/icons-vue';
-import { message } from 'ant-design-vue';
+// import { message } from 'ant-design-vue';
 import { defineComponent, ref } from 'vue';
+// import  AppInfoParser from "app-info-parser";
+ const AppInfoParser = require("app-info-parser/src/apk");
+// interface FileItem {
+//   uid: string;
+//   name?: string;
+//   status?: string;
+//   response?: string;
+//   url?: string;
+// }
 
-interface FileItem {
-  uid: string;
-  name?: string;
-  status?: string;
-  response?: string;
-  url?: string;
-}
-
-interface FileInfo {
-  file: FileItem;
-  fileList: FileItem[];
-}
+// interface FileInfo {
+//   file: FileItem;
+//   fileList: FileItem[];
+// }
 
 export default defineComponent({
   components: {
-    InboxOutlined,
   },
   setup() {
-    const handleChange = (info: FileInfo) => {
-      const status = info.file.status;
-      // if (status !== 'uploading') {
-      //   console.log(info.file, info.fileList);
-      // }
-      if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`);
-      } else if (status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
+    const handleChange = (info) => {
+     const files = info.target.files[0]
+      console.log("🌲🌲🌲",info)
+     const parser = new AppInfoParser(files);
+     parser.parse().then(res=>{
+        console.log("🔥🔥",res,res.icon)
+     })
+     
     };
     return {
       handleChange,
